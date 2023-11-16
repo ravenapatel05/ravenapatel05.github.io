53e0b018a36fbc925fbaf5080daa5515e6df927d
npatel84 <77018379+npatel84@users.noreply.github.com>
2023-11-15 22:28:10 -0700

    LOL nvm

--- src/Quotes.tsx
+++ src/Quotes.tsx
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import './Quotes.scss';
 
-const quotes = [
-  `Ravena, remember that life is a journey. Embrace the reds and whites along the way.`,
-  `Every day is a new canvas for you, Ravena. Paint it with the colors of your choice.`,
-  `Just like your favorite food, happiness is a dish best served daily, Ravena.`,
-  `A long walk is a perfect opportunity to find inspiration and serenity, Ravena.`,
-  `Music is the soundtrack to your life, Ravena. Keep listening to your heart's melody.`,
-  `As you watch the stars, remember that you're as magical as any character in Harry Potter, Ravena.`,
-  // Add more personalized quotes here...
+// const quotes = [
+//   `Ravena, remember that life is a journey. Embrace the reds and whites along the way.`,
+//   `Every day is a new canvas for you, Ravena. Paint it with the colors of your choice.`,
+//   `Just like your favorite food, happiness is a dish best served daily, Ravena.`,
+//   `A long walk is a perfect opportunity to find inspiration and serenity, Ravena.`,
+//   `Music is the soundtrack to your life, Ravena. Keep listening to your heart's melody.`,
+//   `As you watch the stars, remember that you're as magical as any character in Harry Potter, Ravena.`,
+//   // Add more personalized quotes here...
+// ];
+
+const quotes = [ 
+  'LOL NVM 🤣 🫵 🥴 🤷‍♂️'
 ];
 
 const Quotes: React.FC = () => {
